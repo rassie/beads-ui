8e10473f8a083458f5f16b2769476a208e9da66d
Maximilian Antoni <mail@maxantoni.de>
2025-10-22 14:50:22 +0200

    Remove unused "/src"

--- src/utils/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from 'vitest';
-import { sum } from './sum.js';
-
-describe('sum', () => {
-  test('returns sum of numbers', () => {
-    const result_value = sum([1, 2, 3]);
-    expect(result_value).toBe(6);
-  });
-});
